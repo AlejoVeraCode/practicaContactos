98d529ef80740a3bf9a85c479900f1063d0dbc71
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-03-22 11:25:31 -0300

    Add files via upload

---
+++ mongo_config/mongo_config.js
@@ -0,0 +1,44 @@
+import mongoose, { model } from 'mongoose'
+
+const {Schema} = mongoose
+
+const conect =  'mongodb+srv://alejoveracode:Leon.0208@Cluster0.jo8hosd.mongodb.net/mongo_db_contacto?retryWrites=true&w=majority'
+mongoose.connect(conect, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const contactoSchema = new Schema({
+    nombre: String,
+    numero:Number,
+    direccion:String,
+    email:String
+})
+
+const Contacto = mongoose.model('mongodbcontactos', contactoSchema)
+
+const agregarContacto = (req, res) => {
+
+    const contacto = new Contacto({
+        nombre: req.body.nombre,
+        numero :Number(req.body.numero),
+        direccion:req.body.direccion,
+        email:req.body.email
+    })
+    contacto.save()
+    .then(re => {
+        res.redirect('/')
+    })
+}
+
+const todosLosContactos = async (modelo) => {
+    const resultados = await modelo.find({}, (err, docs)=>{
+        return docs
+    })
+    return resultados
+}
+
+const borrarContacto = (modelo, id, res) => {
+    modelo.deleteOne({_id: id}, (err,doc)=> {
+        res.redirect('/')
+    })
+}
+
+export {Contacto, agregarContacto, todosLosContactos, borrarContacto}
