bcb857e796bd21636df950fcb5932be4a69f042a
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-04-19 23:31:37 -0300

    Update index.js
    Se agregan comentarios.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
+//Importamos express desde express
 import express from 'express'
-import {Contacto, agregarContacto, todosLosContactos, borrarContacto } from './mongo_config/mongo_config.js'
-const app = express()
 
-app.set('views', './vistas')
-app.set('view engine', 'ejs')
+//Importamos la funcion de agregar contacto desde el archivo de configuracion de Mongo
+import { agregarContacto, Contacto, todosLosContactos, borrarContacto } from './mongo_config/mongo_config.js';
 
+const app = express ()
+
+//De donde sacaremos las vistas
+
+app.set ('views', './Vistas/');
+app.set ('view engine', 'ejs' )
+
+//Recogemos los archivos estaticos 
 app.use(express.static('./estilos'))
+
+//Pasaremos los datos por post y codificaremos la url 
 app.use(express.urlencoded({extended:true}))
 
+//Escuchamos nuestra app desde el puerto 8000
 
-app.listen('8000', (req, res)=>{
-    console.log('aplicacion en http://localhost:8000')
+app.listen('8000', (req, res) => {
+    console.log ('aplicacion en http://localhost:8000')
 })
-
 app.get('/', async(req, res)=> {
-   let contactos = await todosLosContactos(Contacto)
+    let contactos = await todosLosContactos(Contacto)
     res.render('index', {contactos})
 })
 
