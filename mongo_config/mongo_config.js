3bd321475fd09fb7943612836570ebebc79bfa5b
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-04-19 23:30:39 -0300

    Update mongo_config.js
    Se agregan comentarios

--- mongo_config/mongo_config.js
+++ mongo_config/mongo_config.js
@@ -1,38 +1,48 @@
-import mongoose, { model } from 'mongoose'
+//Importamos mongoDB
+import mongoose from 'mongoose'
 
+//Hacemos una extracccion del objeto (object extractory)
 const {Schema} = mongoose
 
-const conect =  'mongodb+srv://alejoveracode:Leon.0208@Cluster0.jo8hosd.mongodb.net/mongo_db_contacto?retryWrites=true&w=majority'
-mongoose.connect(conect, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const contactoSchema = new Schema({
+//Conectamos con la url de la base de datos.
+const conexion = 'mongodb+srv://alejoveracode:Leon.0208@Cluster0.jo8hosd.mongodb.net/mongo_db_contacto?retryWrites=true&w=majority'
+
+//Configuracion de mongoose
+mongoose.connect(conexion, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Plantilla de la base de datos
+const contactoSchema = new Schema ({
     nombre: String,
-    numero:Number,
-    direccion:String,
-    email:String
+    numero: Number,
+    direccion: String,
+    email: String
 })
 
+//Hacemos un modelo con la plantilla
 const Contacto = mongoose.model('mongodbcontactos', contactoSchema)
 
-const agregarContacto = (req, res) => {
 
-    const contacto = new Contacto({
+const agregarContacto = (req, res) => {
+    const contacto = new Contacto ({
         nombre: req.body.nombre,
-        numero :Number(req.body.numero),
-        direccion:req.body.direccion,
-        email:req.body.email
+        numero: Number (req.body.numero),
+        direccion: req.body.direccion,
+        email: req.body.email
     })
     contacto.save()
-    .then(re => {
+    .then (re => {
         res.redirect('/')
-    })
+    }) .catch(console.error)
 }
 
+//Funcion para que muestre los contactos agregados
 const todosLosContactos = async (modelo) => {
     const resultados = await modelo.find({}).exec();
     return resultados;
 };
 
+//Funcion para que borre los contactos
 const borrarContacto = async (modelo, id, res) => {
     try {
         const resultado = await modelo.deleteOne({_id: id});
@@ -42,6 +52,5 @@ const borrarContacto = async (modelo, id, res) => {
         res.status(500).send('Error al eliminar el contacto');
     }
 }
-
 
 export {Contacto, agregarContacto, todosLosContactos, borrarContacto}
